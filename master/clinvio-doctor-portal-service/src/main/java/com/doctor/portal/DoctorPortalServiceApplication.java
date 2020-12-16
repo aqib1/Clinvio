f0bc347016ca84f233fde04e78c5c873d62593a8
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2020-12-16 07:36:41 +0500

    CLINVIO-DISCOVERY-SERVER-23138 'change mariadb to postgresql''

--- master/clinvio-doctor-portal-service/src/main/java/com/doctor/portal/DoctorPortalServiceApplication.java
+++ master/clinvio-doctor-portal-service/src/main/java/com/doctor/portal/DoctorPortalServiceApplication.java
@@ -2,6 +2,7 @@ package com.doctor.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 /**
  * <p>
@@ -14,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableEurekaClient
 public class DoctorPortalServiceApplication {
 
 	public static void main(String[] args) {
